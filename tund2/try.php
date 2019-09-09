c1cbf504efcad5efed4a3eb8e17645a0e7eab0bb
Raimond Laatspera <RaimondLaatspera@users.noreply.github.com>
2019-09-09 11:42:48 +0300

    L

---
+++ tund2/try.php
@@ -0,0 +1,38 @@
+<?php
+  $userName = "Raimond Laatspera";
+  $fullTimeNow = date("d.m.Y H:i:s");
+  $hourNow = date("H");
+  $partOfDay = "hägune aeg";
+  if($hourNow < 8) {
+	  $partOfDay = "varane hommik";
+  }
+?>
+
+
+<!DOCTYPE html>
+<html lang="et">
+<head>
+  <meta charset="utf-8">
+  <title>
+    <?php
+	echo $userName;
+	?>
+    First</title>
+</head>
+<body>
+<?php
+    echo "<h1>" .$userName ." koolitöö leht</h1>";
+?>
+  <p>See leht on loodud koolis õppetöö raames ja ei sisalda tõsiselt võetavat sisu!</p>
+<hr>
+<p>Lehe avamise hetkel oli aeg:
+<?php
+    echo $fullTimeNow;
+?>
+.</p>
+<?php
+echo "<p>Lehe avamise hetkel oli " .$partOfDay . ".</p>";
+?>
+<hr>
+</body>
+</html>
